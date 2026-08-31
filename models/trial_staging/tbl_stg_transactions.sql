483f2e43bdd450805da9db92e32f952e9a21b1ed
Jigna14 <jdobariya299@gmail.com>
2026-08-31 12:29:54 +0200

    created new files

--- models/trial_staging/tbl_stg_transactions.sql
+++ models/trial_staging/tbl_stg_transactions.sql
@@ -3,12 +3,14 @@ tags = ['staging', 'finance']
 )}}
 
 select
+{{ dbt_utils.generate_surrogate_key(['customer_id'])}} as customer_id_hk,
 customer_id,
 signup_date,
 city,
 country,
 employment_type,
 annual_income,
-created_at
+created_at,
+{{ macro_example('annual_income') }} as income_100
 from {{ source('getsafe', 'customers') }}
 
